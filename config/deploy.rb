d20ccf9f9a96e9deb1b9e072999a16b7ade926e8
Reni-amorim <reni.425@gmail.com>
2023-10-02 02:31:59 +0000

    deploy3 set rvm1 set bin maps

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :bundle_jobs, 1
+set :rvm1_ruby_version, "ruby-2.0.0-p247"
 
 ## Defaults:
 # set :scm,           :git
