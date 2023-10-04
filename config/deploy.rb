b47f7bf0b82d3827605e16515ef4fd6c3819586b
Reni-amorim <reni.425@gmail.com>
2023-10-04 17:44:34 +0000

    set new server ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.3"
 
 # Change these
-server "15.228.170.212", :roles => [:app, :web, :db], :primary => true
+server "15.228.244.232", :roles => [:app, :web, :db], :primary => true
 
 set :repo_url,        'git@github.com:Reni-amorim/blog2.git'
 set :application,     'blog2'
