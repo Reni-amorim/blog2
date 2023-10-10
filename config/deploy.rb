a4fc4273ff56bb268b1b43bb2f9bd11f10e28e81
Reni-amorim <reni.425@gmail.com>
2023-10-10 15:25:22 +0000

    add sshkit.config command map in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :bundle_jobs, 1
-
+SSHKit.config.command_map.prefix[:puma].push("bundle exec")
+SSHKit.config.command_map.prefix[:pumactl].push("bundle exec")
 
 
 ## Defaults:
