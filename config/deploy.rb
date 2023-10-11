4fc697a241562d0e54b8907d08ab694e5f9040e1
Reni-amorim <reni.425@gmail.com>
2023-10-11 00:12:20 +0000

    set puma confit path in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,9 @@ set :application,     'blog2'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+#try to set puma sock
+# The path to your Puma configuration file
+set :puma_config,     '/home/vagrant/Desktop/blog2/config/puma.rb'
 
 ## Dont change #
 set :rvm_map_bins, %w{gem rake ruby rails bundle}
@@ -21,9 +24,6 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-#try to set my puma.sock
-set :puma_socket, "#{shared_path}/tmp/sockets/blog2-puma.sock"
-
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
